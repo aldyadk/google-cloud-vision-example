997de22c3b252463b3db23ef14536e78d52d596f
aldyadk <aldy.andika@gmail.com>
2021-04-15 12:27:03 +0700

    done

---
+++ next.config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+    env: {
+        CV_API_KEY: process.env.CLOUD_VISION_API_KEY
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,76 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function Home() {
+  const [cvData, setCvData] = useState('')
+  const [cvJSONData, setCvJSONData] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFileChange = () => {
+    setIsLoading(true)
+    const file = document.getElementById('test-input').files[0]
+    // console.log(file)
+    if(!file){
+      setIsLoading(false)
+      return
+    }
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    const img = document.getElementById('test-img')
+    reader.onloadend = () => {
+      // console.log(reader.result);
+      img.src = reader.result
+      getCVData(reader.result)
+    }
+    reader.onerror = (err) => {
+      setIsLoading(false)
+      console.log(err)
+    }
+  }
+
+  function getCVData(b64) {
+    b64 = b64.replace('data:image/jpeg;base64,', ''); // remove content type
+    b64 = b64.replace('data:image/jpg;base64,', ''); // remove content type
+    b64 = b64.replace('data:image/png;base64,', ''); // remove content type
+
+    const request = {
+      "requests": [
+        {
+          "image": { "content": b64 },
+          "features": [
+            {
+              "type": "TEXT_DETECTION",
+            }
+          ]
+        }
+      ]
+    };
+
+    axios
+      .post(`https://vision.googleapis.com/v1/images:annotate?key=${process.env.CV_API_KEY}`, request)
+      .then(({ data: { responses } }) => {
+        console.log(responses)
+        if (!responses[0].fullTextAnnotation) {
+          setCvData('no text')
+          setCvJSONData('no text')
+          setIsLoading(false)
+          return
+        }
+        const text = responses[0].fullTextAnnotation.text
+        const jsondata = JSON.stringify(responses[0].textAnnotations, null, 2)
+        setCvData(text)
+        setCvJSONData(jsondata)
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setIsLoading(false)
+        console.log(err)
+      })
+
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,41 +82,25 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <img id="test-img" width={cvData && !isLoading ? 400 : 0} height={cvData && !isLoading ? 400 : 0} />
+        <input id="test-input" type="file" accept=".jpg,.jpeg,.png" onChange={handleFileChange} />
+        {isLoading ? (
+          <h1>Loading.....</h1>
+        ) : (
+          <>
+            <button onClick={() => alert('lakukan upload!')}>Misalkan Ini Button Upload</button>
+            <div style={{ width: '80vw', margin: 'auto', minHeight: '50vh', background: '#ADD8E6' }}>
+              {cvData.split('\n').map((v, i) => (
+                <div key={i}>{v}</div>
+              ))}
+            </div>
+            <div style={{ width: '80vw', margin: 'auto', minHeight: '100vh', background: '#1F75FE', color: 'white' }}>
+              {cvJSONData.split('\n').map((v, i) => (
+                <div key={i}>{v}</div>
+              ))}
+            </div>
+          </>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -60,6 +113,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
-    </div>
+    </div >
   )
 }
